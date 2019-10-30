9d3d4c10621fda38d0905424eca591d9ea20de45
dongzhen1236 <dongzhen1236@163.com>
2019-10-30 22:46:11 +0800

    update index.html

--- assets/js/commonJs.js
+++ assets/js/commonJs.js
@@ -1,25 +1,25 @@
 
 document.write('<!-----------  公共js start----------->');
 document.write('' +
-    '<script src="assets/js/jquery-1.11.1.min.js"></script>\n' +
-    '<script src="assets/js/bootstrap.min.js"></script>\n' +
-    '<script src="assets/js/chart.min.js"></script>\n' +
-    '<script src="assets/js/chart-data.js"></script>\n' +
-    '<script src="assets/js/easypiechart.js"></script>\n' +
-    '<script src="assets/js/easypiechart-data.js"></script>\n' +
-    '<script src="assets/js/bootstrap-datepicker.js"></script>\n' +
-    // '<script src="assets/js/bootstrap-table.js"></script>\n' +
-    '<script src="assets/js/bootstrap-table.min.js"></script>\n' +
-    '<script src="assets/js/custom.js"></script>\n' +
-    '<script src="assets/js/bootstrap-table-zh-CN.min.js"></script>\n' +
+    '<script src="../assets/js/jquery-1.11.1.min.js"></script>\n' +
+    '<script src="../assets/js/bootstrap.min.js"></script>\n' +
+    '<script src="../assets/js/chart.min.js"></script>\n' +
+    '<script src="../assets/js/chart-data.js"></script>\n' +
+    '<script src="../assets/js/easypiechart.js"></script>\n' +
+    '<script src="../assets/js/easypiechart-data.js"></script>\n' +
+    '<script src="../assets/js/bootstrap-datepicker.js"></script>\n' +
+    // '<script src="../assets/js/bootstrap-table.js"></script>\n' +
+    '<script src="../assets/js/bootstrap-table.min.js"></script>\n' +
+    '<script src="../assets/js/custom.js"></script>\n' +
+    '<script src="../assets/js/bootstrap-table-zh-CN.min.js"></script>\n' +
 
     // 引入toastr
     '<!--toastr.js uses jQuery, so jQuery should get loaded before toastr.js-->\n' +
     // '<script src="plugins/toastr/jquery.min.js"></script>\n' +
-    '<script src="plugins/toastr/toastr.min.js"></script>\n' +
+    '<script src="../plugins/toastr/toastr.min.js"></script>\n' +
 
     // 公用js依赖上面文件
-    '<script src="assets/js/ewin.js"></script>\n' +
-    '<script src="assets/js/common.js"></script>\n' +
+    '<script src="../assets/js/ewin.js"></script>\n' +
+    '<script src="../assets/js/common.js"></script>\n' +
     '');
 document.write('<!-----------  公共js end----------->');
